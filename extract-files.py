d094b97fa3fd1922a4cceb3787749e0dc6aaff13
Stella Diaz <electi@yzu.moe>
2026-02-16 20:16:20 -0500

    jagar: Configure media seccomp policies
    Change-Id: I57b3ab3c7c9e6e20f51cce52fa278faff46e3824
    Signed-off-by: Stella Diaz <electi@yzu.moe>
    
    jagar: Patchelf C2 service to link against libavservices_minijail
    
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Change-Id: I4cddc98e91724c86e7761fee41d4f9f5554c7203
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    
    jagar: Link media.c2 service against v33 libstagefright_foundation
    
    * Fixes crashes and high CPU usage
    
    Change-Id: I60c07d10c2db9335d20c6dbaaaeacc9313aebf06
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- extract-files.py
+++ extract-files.py
@@ -111,6 +111,12 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/teed'
     ): blob_fixup()
         .binary_regex_replace(b'ro.product.model', b'ro.build.product'),
+    (
+        'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
+        'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b',
+    ): blob_fixup()
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so')
+        .add_needed('libstagefright_foundation-v33.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
