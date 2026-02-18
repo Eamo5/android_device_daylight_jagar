5f68d60cef4cc8b48b5b89c270d6ea494e8fcd54
Stella Diaz <electi@yzu.moe>
2026-02-17 23:13:13 -0500

    jagar: configs: Import kcm/kl/idc from stock
    Change-Id: I3f3ebbaeaaa3794fb4fd7c0809ffc4157b7e6864
    Signed-off-by: Stella Diaz <electi@yzu.moe>

--- device.mk
+++ device.mk
@@ -94,6 +94,12 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/jagar_keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/jagar_keyboard.idc \
+    $(LOCAL_PATH)/configs/keylayout/jagar_keyboard.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/jagar_keyboard.kl \
+    $(LOCAL_PATH)/configs/keychars/jagar_keyboard.kcm:$(TARGET_COPY_OUT_VENDOR)/usr/keychars/jagar_keyboard.kcm
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
